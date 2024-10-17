20fa55ffe154c975dace4dbad6089592b30db83e
Alex <74208897+AlexTheLion123@users.noreply.github.com>
2024-10-18 01:45:39 +0200

    update list layout

--- src/components/GameCard/GameCardList.tsx
+++ src/components/GameCard/GameCardList.tsx
@@ -66,68 +66,91 @@ export default function GameCardList(props: Readonly<GameCardListProps>) {
   )
   const { theme } = useTheme()
 
+  const buttonClassName = 'rounded-xl max-w-[100px]'
+
   return (
     <Link href={`/event/${gameId}`}>
       <div
         className={clsx(
           'hover:bg-gradient-to-l hover:from-[#ff65a6] hover:via-[#b37ed3] hover:to-[#5e64eb] p-[15px] h-full',
           className,
-          'mb-1 rounded-lg min-h-[100px] justify-between flex',
-          theme === 'dark' ? 'bg-[#262a31]' : 'bg-[#ADD6FF]' // Change based on the theme
+          'mb-1 rounded-lg min-h-[100px]',
+          theme === 'dark' ? 'bg-[#262a31]' : 'bg-[#ADD6FF]', // Change based on the theme
+          'grid grid-cols-5 auto-rows-auto gap-4'
         )}
       >
-        <div className="w-[40%] grid flex-end">
-          <div className="flex flex-col flex-1 row-start-2">
-            <Participant
-              {...participants[0]}
-              className="flex-row"
-              size={'xs'}
-            />
-            <Participant
-              {...participants[1]}
-              className="flex-row"
-              size={'xs'}
-            />
-          </div>
-          <div className="text-[10px] font-bold col-start-1 row-start-3 flex h-6 gap-2 items-center">
-            <div className="">{formattedStartAt.date}</div>
-            <div className="bg-[#FFFFFF0D] rounded-lg h-full flex items-center px-1 justify-center">
-              {formattedStartAt.time}
-            </div>
+        <div className="col-start-1 row-start-2 flex flex-col flex-1">
+          <Participant
+            {...participants[0]}
+            className="flex-row"
+            size={'xs'}
+          />
+          <Participant
+            {...participants[1]}
+            className="flex-row"
+            size={'xs'}
+          />
+        </div>
+
+        <div className="max-lg:hidden col-start-1 row-start-3  text-[10px] font-bold flex h-6 gap-2 items-center">
+          <div className="">{formattedStartAt.date}</div>
+          <div className="bg-[#FFFFFF0D] rounded-lg h-full flex items-center px-1 justify-center">
+            {formattedStartAt.time}
           </div>
         </div>
-        <div className="w-[60%]">
-          <div className="gap-5 justify-between">
-            {markets.slice(0, 2).map((market, marketIndex) => (
-              <div key={marketIndex} className="mb-2">
-                {market.outcomeRows.map((outcomeRow, rowIndex) => (
-                  <div key={rowIndex} className="gap-2">
-                    <div className="row-start-1 col-start-3 flex justify-around flex-1">
-                      {outcomeRow.map((outcome, index) => (
-                        <div>{outcome.selectionName}</div>
-                      ))}
-                    </div>
-                    <div className="flex-1 text-center flex justify-between gap-2 row-start-2 col-start-3 items-center">
-                      {outcomeRow.map((outcome, index) => (
-                        <OutcomeButton
-                          index={index}
-                          key={outcome.outcomeId}
-                          text={''}
-                          outcome={outcome}
-                          onSelectOutcome={() => onSelectOutcome(outcome)}
-                          isPlaced={checkIsBetPlaced(outcome)}
-                          textAbove={true}
-                        />
-                      ))}
-                    </div>
-                  </div>
-                ))}
-              </div>
-            ))}
-          </div>
-          <div className="row-start-3 col-start-3 text-end">
-            {markets.length} {'>'}
-          </div>
+
+        <div className="max-lg:hidden row-start-2 col-start-2 flex items-center justify-end">
+          {markets[1]?.outcomeRows[0].length < 3 && markets[1]?.name}
+        </div>
+        <div className="max-lg:hidden row-start-1 col-start-3 flex justify-around items-center">
+          {markets[1]?.outcomeRows[0].length < 3 && markets[1]?.outcomeRows[0].map((outcome, index) => (
+            <div key={index}>{outcome.selectionName}</div>
+          ))}
+        </div>
+        <div className="max-lg:hidden row-start-2 col-start-3 flex justify-around items-center">
+          {markets[1]?.outcomeRows[0].length < 3 && markets[1]?.outcomeRows[0].map((outcome, index) => (
+            <div>
+              <OutcomeButton
+                index={index}
+                key={outcome.outcomeId}
+                text={''}
+                outcome={outcome}
+                onSelectOutcome={() => onSelectOutcome(outcome)}
+                isPlaced={checkIsBetPlaced(outcome)}
+                textAbove={true}
+                className={buttonClassName}
+              />
+            </div>
+          ))}
+        </div>
+
+        <div className="max-lg:hidden row-start-2 col-start-4 flex items-center justify-end">
+          {markets[0]?.name}
+        </div>
+        <div className="row-start-1 col-start-5 flex justify-around items-center">
+          {markets[0]?.outcomeRows[0].map((outcome, index) => (
+            <div key={index}>{outcome.selectionName}</div>
+          ))}
+        </div>
+        <div className="row-start-2 col-start-5 flex justify-around items-center">
+          {markets[0]?.outcomeRows[0].map((outcome, index) => (
+            <div>
+              <OutcomeButton
+                index={index}
+                key={outcome.outcomeId}
+                text={''}
+                outcome={outcome}
+                onSelectOutcome={() => onSelectOutcome(outcome)}
+                isPlaced={checkIsBetPlaced(outcome)}
+                textAbove={true}
+                className={buttonClassName}
+              />
+            </div>
+          ))}
+        </div>
+
+        <div className="row-start-3 col-start-5 text-end">
+          {markets.length} {'>'}
         </div>
       </div>
     </Link>
